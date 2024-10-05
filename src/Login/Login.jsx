85f68d5dced4dfcbfb6ca5f368987154694ce6f4
Sazzadur Rahman <sazzadurrahman000@gmail.com>
2024-10-05 21:31:45 +0600

    impliment google githoub

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,25 +1,86 @@
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import {
+  getAuth,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { useState } from "react";
 import app from "../firebase/firebase.init";
+app;
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const auth = getAuth();
-  const provider = new GoogleAuthProvider(app);
+  const GootleProvider = new GoogleAuthProvider();
+  const GitHubProvider = new GithubAuthProvider();
 
   const handleGoogleSignIn = () => {
-    signInWithPopup(auth, provider)
+    signInWithPopup(auth, GootleProvider)
       .then((result) => {
-        const user = result.user;
-        console.log(user);
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
       })
       .catch((error) => {
         console.log("error", error);
       });
   };
 
+  const handleSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        // An error happened.
+      });
+  };
+
+  const handleGitHubLogin = () => {
+    signInWithPopup(auth, GitHubProvider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        console.log(loggedInUser);
+        setUser(loggedInUser);
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  const handleGitHubSignOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log("error", error);
+        // An error happened.
+      });
+  };
+
   return (
     <div>
       <br />
-      <button onClick={handleGoogleSignIn}>Google login</button>
+      {/* user ? logout : sign in */}
+      {user ? (
+        <>
+          <button onClick={handleSignOut}>SignOut</button>
+          <button onClick={handleGitHubSignOut}>GitHub SignOut</button>
+        </>
+      ) : (
+        <div>
+          <button onClick={handleGoogleSignIn}>Google login</button>
+          <button onClick={handleGitHubLogin}>GitHub login</button>
+        </div>
+      )}
+      <h3>User: {user?.displayName}</h3>
+      <p>Email: {user?.email}</p>
+      <img src={user?.photoURL} alt="" />
     </div>
   );
 };
